0ea41582fe1f87ad62cbeb78e175232ab30566e9
isfandiyar.rashidzade <isfandiyar.rashidzade@testdevlab.com>
2024-01-25 16:38:21 +0400

    AndroidEntryPoint

--- app/src/main/java/az/isfan/spoonsgame/Activity/MainActivity.kt
+++ app/src/main/java/az/isfan/spoonsgame/Activity/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import az.isfan.spoonsgame.Navigation.NavGraph
 import az.isfan.spoonsgame.ui.theme.SpoonsGameTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
