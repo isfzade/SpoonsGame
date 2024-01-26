2f8935aba9a02df14dda98eb69af06b9814c03bc
isfandiyar.rashidzade <isfandiyar.rashidzade@testdevlab.com>
2024-01-26 12:27:00 +0400

    input

--- app/src/main/java/az/isfan/spoonsgame/Screens/Game/components/RightChair.kt
+++ app/src/main/java/az/isfan/spoonsgame/Screens/Game/components/RightChair.kt
@@ -1,8 +1,11 @@
 package az.isfan.spoonsgame.Screens.Game.components
 
 import androidx.compose.runtime.Composable
+import az.isfan.spoonsgame.Data.Models.PlayerData
 
 @Composable
-fun RightChair() {
+fun RightChair(
+    playerData: PlayerData
+) {
 
 }
