1564c01da59789aadb0a7d3031efbdc4aba2821a
isfandiyar.rashidzade <isfandiyar.rashidzade@testdevlab.com>
2024-01-26 12:28:15 +0400

    input

--- app/src/main/java/az/isfan/spoonsgame/Screens/Game/components/GameTable.kt
+++ app/src/main/java/az/isfan/spoonsgame/Screens/Game/components/GameTable.kt
@@ -1,8 +1,12 @@
 package az.isfan.spoonsgame.Screens.Game.components
 
 import androidx.compose.runtime.Composable
+import az.isfan.spoonsgame.Data.Models.CardData
 
 @Composable
-fun GameTable() {
+fun GameTable(
+    availableCards: List<CardData>,
+    discardedCards: List<CardData>
+) {
 
 }
