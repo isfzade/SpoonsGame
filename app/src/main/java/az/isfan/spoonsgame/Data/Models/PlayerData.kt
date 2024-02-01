773bc6290ed783d16df4e77d81e81622bcd07d7d
isfandiyar.rashidzade <isfandiyar.rashidzade@testdevlab.com>
2024-02-01 14:56:04 +0400

    player data

--- app/src/main/java/az/isfan/spoonsgame/Data/Models/PlayerData.kt
+++ app/src/main/java/az/isfan/spoonsgame/Data/Models/PlayerData.kt
@@ -8,94 +8,19 @@ import kotlinx.coroutines.flow.update
 
 data class PlayerData(
     val name: String,
-    val isLocalUser: Boolean,
     val chair: ChairEnum,
+    val cards: List<CardData> = emptyList(),
+    val isLocalUser: Boolean = false,
+    val firstPlayer: Boolean = false,
+    val kicked: Boolean = false,
+    val lettersSize: Int = 0,
 ) {
     private val TAG = "isf_PlayerData"
 
-    private val _playTurn = MutableStateFlow(false)
-    val playTurn = _playTurn.asStateFlow()
-
-    private val _cards = MutableStateFlow<List<CardData>>(emptyList())
-    val cards = _cards.asStateFlow()
-
-    private val _firstPlayerInRound = MutableStateFlow(false)
-    val firstPlayerInRound = _firstPlayerInRound.asStateFlow()
-
-    private val _lastPlayerInRound = MutableStateFlow(false)
-    val lastPlayerInRound = _lastPlayerInRound.asStateFlow()
-
-    private val _isPlaying = MutableStateFlow(true)
-    val isPlaying = _isPlaying.asStateFlow()
-
-    private val _lettersCollected = MutableStateFlow(0)
-    val lettersCollected = _lettersCollected.asStateFlow()
-
-    fun setCards(newCards: List<CardData>){
-        _cards.update { newCards }
-    }
-
-    fun addCard(card: CardData) {
-        Log.i(TAG, "addCard: card=$card, player=$this")
-
-        setCards(cards.value+card)
-        card.setHolder(name)
-    }
-
-    fun removeCard(card: CardData) {
-        Log.i(TAG, "removeCard: card=$card, player=$this")
-
-        val newCards = cards.value.filter { !(it.rank == card.rank && it.suit == card.suit) }
-        card.setHolder(null)
-        setCards(newCards)
-    }
-
-    fun removeAllCards() {
-        Log.i(TAG, "removeAllCards: player=$this")
-
-        _cards.update { emptyList() }
-    }
-
-    fun setPlayTurn(isPlayerTurn: Boolean) {
-        Log.i(TAG, "setPlayTurn: isPlayerTurn=$isPlayerTurn, player=$this")
-
-        _playTurn.update { isPlayerTurn }
-    }
-
-    fun setFirstPlayerInRounds(isFirst: Boolean) {
-        Log.i(TAG, "setFirstPlayerInRounds: isFirst=$isFirst, player=$this")
-
-        _firstPlayerInRound.update { isFirst }
-    }
-
-    fun setLastPlayerInRounds(isLast: Boolean) {
-        Log.i(TAG, "setLastPlayerInRounds: isLast=$isLast, player=$this")
-
-        _lastPlayerInRound.update { isLast }
-    }
-
-    fun setIsPlaying(playing: Boolean) {
-        Log.i(TAG, "setIsPlaying: playing=$playing, player=$this")
-
-        _isPlaying.update { playing }
-    }
-
-    fun setLettersCollected(size: Int) {
-        Log.i(TAG, "setLettersCollected: size=$size, player=$this")
-
-        _lettersCollected.update { size }
-    }
-
-    fun incLetterCollected() {
-        Log.i(TAG, "incLetterCollected: player = $this")
-
-        _lettersCollected.update { it+1 }
-    }
-
     fun has4EqualCards(): Boolean {
-        Log.i(TAG, "has4EqualCards: player=$this")
+        Log.i(TAG, "has4EqualCards: name=$name")
 
-        if (cards.value.size != 4) return false
-        return cards.value.count {it.suit == cards.value.first().suit} == 4
+        if (cards.size != 4) return false
+        return cards.count { it.suit == cards.first().suit } == 4
     }
 }
