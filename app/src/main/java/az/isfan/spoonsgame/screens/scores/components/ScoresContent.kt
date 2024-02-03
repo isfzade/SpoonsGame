903331699dc55dcb2993efc6b6b461ecd33adea3
isfzade <irashidzade@gmail.com>
2024-02-03 13:18:39 +0400

    padding

--- app/src/main/java/az/isfan/spoonsgame/screens/scores/components/ScoresContent.kt
+++ app/src/main/java/az/isfan/spoonsgame/screens/scores/components/ScoresContent.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import az.isfan.spoonsgame.R
 import az.isfan.spoonsgame.data.models.GameData
 import az.isfan.spoonsgame.screens.common.TopBarScreen
@@ -38,6 +39,7 @@ fun ScoresContent(
         Box(
             modifier = Modifier
                 .padding(padding)
+                .padding(top = 10.dp)
                 .fillMaxWidth()
         ) {
             ItemsInScores(games)
