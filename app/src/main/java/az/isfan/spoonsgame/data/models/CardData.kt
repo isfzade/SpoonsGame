09afa3b1338041ad098bca0961f8c39ccc7ddf21
isfzade <irashidzade@gmail.com>
2024-02-04 15:05:44 +0400

    no need equal override for card

--- app/src/main/java/az/isfan/spoonsgame/data/models/CardData.kt
+++ app/src/main/java/az/isfan/spoonsgame/data/models/CardData.kt
@@ -6,11 +6,4 @@ import az.isfan.spoonsgame.data.enums.SuitEnum
 data class CardData(
     val suit: SuitEnum,
     val rank: RankEnum,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is CardData) return false
-
-        return suit == other.suit && rank == other.rank
-    }
-}
+)
