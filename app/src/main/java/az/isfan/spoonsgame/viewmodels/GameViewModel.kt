73c15117046c1faf28db2673ad4c7144c4f973ea
isfzade <irashidzade@gmail.com>
2024-02-04 14:51:19 +0400

    fix delay

--- app/src/main/java/az/isfan/spoonsgame/viewmodels/GameViewModel.kt
+++ app/src/main/java/az/isfan/spoonsgame/viewmodels/GameViewModel.kt
@@ -34,7 +34,7 @@ class GameViewModel @Inject constructor(
     private val TAG = "isf_GameViewModel"
 
     private val DURATION_TO_TAKE_SPOON = 1000L //milliseconds
-    private val DELAY_TURN = 1000L //milliseconds
+    private val DELAY_TURN = 300L //milliseconds
 
     private val _isGameReady = MutableStateFlow<Cavab<Boolean>>(Cavab.StandBy)
     val isGameReady = _isGameReady.asStateFlow()
@@ -160,7 +160,6 @@ class GameViewModel @Inject constructor(
         viewModelScope.launch {
             discardCard(card)
             updateMoods()
-            delay(DELAY_TURN)
             if (isRoundFinished()) {
                 endRound()
                 giveLetter()
